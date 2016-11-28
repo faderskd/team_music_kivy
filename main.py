d210efce5df7cfac23d004f490b817e1fab0dea1
daniel.faderski <daniel.faderski@jmr.pl>
2016-11-28 02:25:39 +0100

    Base layout

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
-from kivy.uix.screenmanager import ScreenManager, Screen
 
 
 Builder.load_file('design/root.kv')
@@ -15,26 +14,6 @@ class RootWidget(BoxLayout):
 class NotLoggedBoxLayout(BoxLayout):
     def switch_screen(self, screen):
         self.screen_manager.switch_to(screen)
-
-
-class NotLoggedScreenManager(ScreenManager):
-    screen_manager = ObjectProperty(None)
-
-
-class NotLoggedMainPageScreen(Screen):
-    pass
-
-
-class NotLoggedMenuScreen(Screen):
-    screen_manager = ObjectProperty(None)
-
-
-class LoginForm(Screen):
-    pass
-
-
-class RegisterForm(Screen):
-    pass
 
 
 class TeamMusicApp(App):
